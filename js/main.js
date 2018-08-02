49df948814d6ca2450b86b9e915a3bdeb640deb0
BtoBastian <bastianoppermann1997@gmail.com>
2018-08-02 13:00:10 +0200

    Added support for searching JavaDoc methods

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-let wikiJson = [];
+// TODO get base url dynamically
+let javadocBaseUrl = 'https://docs.javacord.org/api/build/4655/';
 
 // Get the wiki data
-$.get("/api/wiki.json", function (data) {
+let wikiJson = [];
+$.get('/api/wiki.json', function (data) {
     wikiJson = data;
 }, 'json');
 
-let searchBox = document.getElementById("search-box");
+// Get JavaDoc methods
+$.getScript(javadocBaseUrl + 'member-search-index.js');
+
+let searchBox = document.getElementById('search-box');
 let lastSearchTerm = null;
 
 // Removes suggestions
@@ -37,14 +42,23 @@ function displaySuggestions() {
     }
 
     // Add dropdown menu
-    autocompleteList = document.createElement("div");
-    autocompleteList.setAttribute("id", "autocomplete-list");
-    autocompleteList.setAttribute("class", "dropdown-menu");
+    autocompleteList = document.createElement('div');
+    autocompleteList.setAttribute('id', 'autocomplete-list');
+    autocompleteList.setAttribute('class', 'dropdown-menu');
     searchBox.parentElement.appendChild(autocompleteList);
 
     // Fill dropdown menu
     let searchResults = searchWikiArticles(searchBox.value);
-    autocompleteList.innerHTML += '<div class="dropdown-header"><b>Wiki-Articles</b></div>';
+    autocompleteList.innerHTML += '<div class="dropdown-header"><strong>Wiki-Articles</strong></div>';
+    if (searchResults.length <= 0) {
+        autocompleteList.innerHTML += '<div class="dropdown-header">No results</div>';
+    }
+    for (let i = 0; i < searchResults.length; i++) {
+        autocompleteList.appendChild(searchResults[i]);
+    }
+
+    searchResults = searchJavadocMethods(searchBox.value);
+    autocompleteList.innerHTML += '<div class="dropdown-header"><strong>Methods</strong></div>';
     if (searchResults.length <= 0) {
         autocompleteList.innerHTML += '<div class="dropdown-header">No results</div>';
     }
@@ -61,7 +75,7 @@ function searchWikiArticles(search) {
     let counter = 0;
     for (let i = 0; i < wikiJson.length; i++) {
         if (wikiJson[i].title.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
-            let autocompleteElement = document.createElement("div");
+            let autocompleteElement = document.createElement('div');
             autocompleteElement.innerHTML = '<a class="dropdown-item" href="' + wikiJson[i].url + '" onclick="location.href = \'' + wikiJson[i].url + '\'">' + highlightWordsNoCase(wikiJson[i].title, searchBox.value) + '</a>';
             searchResults.push(autocompleteElement);
             counter++;
@@ -73,21 +87,45 @@ function searchWikiArticles(search) {
     return searchResults;
 }
 
-// Escapes special characters
+// Search for javadooc methods and return an array with html elements
+function searchJavadocMethods(search) {
+    let searchResults = [];
+    let counter = 0;
+    for (let i = 0; i < memberSearchIndex.length; i++) {
+        let package = memberSearchIndex[i].p;
+        let className = memberSearchIndex[i].c;
+        let methodName = memberSearchIndex[i].l;
+        let url = memberSearchIndex[i].url;
+        let fullName = className + '#' + methodName;
+        url = javadocBaseUrl + package.replace(/\./g, '/') + '/' + className + '.html#' + (url === undefined ? methodName.replace('(', '-').replace(')', '-') : url);
+        if (fullName.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+            let autocompleteElement = document.createElement('div');
+            autocompleteElement.innerHTML = '<a class="dropdown-item" href="' + url + '" onclick="location.href = \'' + url + '\'">' + highlightWordsNoCase(fullName, searchBox.value) + '</a>';
+            searchResults.push(autocompleteElement);
+            counter++;
+        }
+        if (counter >= 5) {
+            break;
+        }
+    }
+    return searchResults;
+}
+
+// Escapes regex characters
 RegExp.escape = function(str) {
     var specials = /[.*+?|()\[\]{}\\$^]/g; // .*+?|()[]{}\$^
-    return str.replace(specials, "\\$&");
+    return str.replace(specials, '\\$&');
 };
 
 // Highlights the given word
 function highlightWordsNoCase(line, word) {
-    var regex = new RegExp("(" + RegExp.escape(word) + ")", "gi");
-    return line.replace(regex, "<strong>$1</strong>");
+    var regex = new RegExp('(' + RegExp.escape(word) + ')', 'gi');
+    return line.replace(regex, '<strong>$1</strong>');
 }
 
 // Add some event listeners to update the suggestions
-searchBox.addEventListener("focusin", () => displaySuggestions());
-searchBox.addEventListener("focusout", () => setTimeout(() => removeSuggestions(), 200));
-$("#search-box").on('change keydown paste input', function() {
+searchBox.addEventListener('focusin', () => displaySuggestions());
+searchBox.addEventListener('focusout', () => setTimeout(() => removeSuggestions(), 200));
+$('#search-box').on('change keydown paste input', function() {
     displaySuggestions();
 });
